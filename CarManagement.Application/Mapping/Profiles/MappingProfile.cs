5934cf83f1f0c23407d3200d2034547edc4f8fb9
pawtes <paweltes19@gmail.com>
2021-10-15 22:56:40 +0200

    Add mapping profiles

---
+++ CarManagement.Application/Mapping/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CarManagement.Application.Features.Bookings.Commands.CreateBooking;
+using CarManagement.Domain.Entities;
+
+namespace CarManagement.Application.Mapping.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Booking, CreateBookingCommand>().ReverseMap();
+        }
+    }
+}
