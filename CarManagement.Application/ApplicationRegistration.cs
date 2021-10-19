36f91f80ba0c9ac6ce1a37201bd529e944af6bca
pawtes <paweltes19@gmail.com>
2021-10-19 22:33:33 +0200

    Add application registration

---
+++ CarManagement.Application/ApplicationRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarManagement.Application
+{
+    public static class ApplicationInstallation
+    {
+        public static IServiceCollection AddCarManagementApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
