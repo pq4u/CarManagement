c0ec42da676d3f03e47e70e1f82f527cb8e4a03c
pawtes <paweltes19@gmail.com>
2021-10-20 22:32:58 +0200

    Add api project

---
+++ CarManagement.Api/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using CarManagement.Application;
+using CarManagement.Persistence.EF;
+using CarManagement.Persistence.EntityFramework;
+
+namespace CarManagement.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "TepperAPI"
+                });
+            });
+            
+            services.AddCarManagementApplication();
+            services.AddCarManagementPersistenceEfServices(Configuration);
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Open",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyHeader().AllowAnyMethod());
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "TepperApi");
+            });
+                
+            app.UseCors("Open");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
