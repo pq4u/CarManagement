a713a506b61a40d986da4d102196e9c55ae1af47
pawtes <paweltes19@gmail.com>
2021-10-26 21:41:08 +0200

    Add get brands export query

---
+++ CarManagement.Application/Features/Brands/Queries/GetBrandsExport/BrandExportFileVm.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace CarManagement.Application.Features.Brands.Queries.GetBrandsExport
+{
+    public class BrandExportFileVm
+    {
+        public string BrandExportFileName { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
