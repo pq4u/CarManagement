5c7c6d7c7306ae5a5b89ffda14e6968e6519111a
pawtes <paweltes19@gmail.com>
2021-10-31 16:28:18 +0100

    Add infrastructure project

---
+++ CarManagement.Infrastructure/FileExport/CsvExporter.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CarManagement.Application.Contracts.Infrastructure;
+using CarManagement.Application.Features.Brands.Queries.GetBrandsExport;
+using CarManagement.Application.Features.Colours.Queries.GetColoursExport;
+using CarManagement.Application.Features.Customers.Queries.GetCustomersExport;
+using CarManagement.Application.Features.Models.Queries.GetModelsExport;
+using CarManagement.Application.Features.Vehicles.Queries.GetVehiclesExport;
+using CsvHelper;
+
+namespace CarManagement.Infrastructure.FileExport
+{
+    public class CsvExporter : ICsvExporter
+    {
+        public byte[] ExportBookingsToCsv(List<BookingExportDto> bookingExportDtos)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter);
+                csvWriter.WriteRecords(bookingExportDtos);
+            }
+
+            return memoryStream.ToArray();
+        }
+        
+        public byte[] ExportBrandsToCsv(List<BrandExportDto> brandExportDtos)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter);
+                csvWriter.WriteRecords(brandExportDtos);
+            }
+
+            return memoryStream.ToArray();
+        }
+        
+        public byte[] ExportCustomersToCsv(List<CustomerExportDto> customerExportDtos)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter);
+                csvWriter.WriteRecords(customerExportDtos);
+            }
+
+            return memoryStream.ToArray();
+        }
+        
+        
+        public byte[] ExportColoursToCsv(List<ColourExportDto> colourExportDtos)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter);
+                csvWriter.WriteRecords(colourExportDtos);
+            }
+
+            return memoryStream.ToArray();
+        }
+        
+        public byte[] ExportModelsToCsv(List<ModelExportDto> modelExportDtos)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter);
+                csvWriter.WriteRecords(modelExportDtos);
+            }
+
+            return memoryStream.ToArray();
+        }
+        
+        public byte[] ExportVehiclesToCsv(List<VehicleExportDto> vehicleExportDtos)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter);
+                csvWriter.WriteRecords(vehicleExportDtos);
+            }
+
+            return memoryStream.ToArray();
+        }
+    }
+}
---
+++ CarManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using CarManagement.Application.Contracts.Infrastructure;
+using CarManagement.Infrastructure.FileExport;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarManagement.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddTransient<ICsvExporter, CsvExporter>();
+
+            return services;
+        }
+
+    }
+}
