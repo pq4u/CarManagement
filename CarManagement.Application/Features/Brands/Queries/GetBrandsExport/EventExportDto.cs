6d67b78db4b86c42b8c54b8cdcff0f4c73dd5126
pawtes <paweltes19@gmail.com>
2021-11-02 19:42:58 +0100

    Delete useless files

--- CarManagement.Application/Features/Brands/Queries/GetBrandsExport/EventExportDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CarManagement.Application.Features.Brands.Queries.GetBrandsExport
-{
-    public class BrandExportDto
-    {
-        public int BrandId { get; set; }
-        public string Name { get; set; }
-    }
-}
