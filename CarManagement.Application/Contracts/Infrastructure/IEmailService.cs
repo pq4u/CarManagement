815cf21443093b50131dee43c0cb54622e7739e3
pawtes <paweltes19@gmail.com>
2021-11-02 19:44:28 +0100

    Add sending email service

---
+++ CarManagement.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CarManagement.Application.Models.Mail;
+
+namespace CarManagement.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        public abstract Task<bool> SendEmail(Email email);
+    }
+}
