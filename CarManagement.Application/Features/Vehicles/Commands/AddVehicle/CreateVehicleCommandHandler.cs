699462b601201e6c14ddb765249c4d455ecf4459
pawtes <paweltes19@gmail.com>
2021-11-06 20:32:30 +0100

    Add sending an email after the action on the vehicle

--- CarManagement.Application/Features/Vehicles/Commands/AddVehicle/CreateVehicleCommandHandler.cs
+++ CarManagement.Application/Features/Vehicles/Commands/AddVehicle/CreateVehicleCommandHandler.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using CarManagement.Application.Contracts.Infrastructure;
 using CarManagement.Application.Contracts.Persistence;
+using CarManagement.Application.Models.Mail;
 using CarManagement.Domain.Entities;
 using MediatR;
 
@@ -11,6 +14,7 @@ namespace CarManagement.Application.Features.Vehicles.Commands.AddVehicle
     {
         private readonly IVehicleRepository _vehicleRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
         public CreateVehicleCommandHandler(IMapper mapper, IVehicleRepository vehicleRepository)
         {
@@ -29,6 +33,22 @@ namespace CarManagement.Application.Features.Vehicles.Commands.AddVehicle
             var vehicle = _mapper.Map<Vehicle>(request);
             vehicle = await _vehicleRepository.AddAsync(vehicle);
 
+            var email = new Email()
+            {
+                To = "example@name.com",
+                Body = $"A new vehicle has been created: {request}",
+                Subject = $"New vehicle: {request}"
+            };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
             return new CreateVehicleCommandResponse(vehicle.VehicleId);
         }
     }
