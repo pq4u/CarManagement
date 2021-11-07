16c27e93748821d16261f2bdd2811ad30cd84d8d
pawtes <paweltes19@gmail.com>
2021-11-07 19:35:16 +0100

    Add email service to infrastructure registration

--- CarManagement.Infrastructure/InfrastructureServiceRegistration.cs
+++ CarManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,5 +1,7 @@
 using CarManagement.Application.Contracts.Infrastructure;
+using CarManagement.Application.Models.Mail;
 using CarManagement.Infrastructure.FileExport;
+using CarManagement.Infrastructure.Mail;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,7 +12,10 @@ namespace CarManagement.Infrastructure
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
             IConfiguration configuration)
         {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            
             services.AddTransient<ICsvExporter, CsvExporter>();
+            services.AddTransient<IEmailService, EmailService>();
 
             return services;
         }
